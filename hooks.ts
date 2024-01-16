a4b77acd7ae0f7de59257162d9973a6fa8cb9471
Lee <leesheppard2404@gmail.com>
2024-01-16 16:31:50 +0100

    feat: Add color persistence using AsyncStorage

--- hooks.ts
+++ hooks.ts
@@ -1,10 +1,29 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Color } from './types';
 import 'react-native-get-random-values';
 import { v4 as uuidv4 } from 'uuid';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const useColors = () => {
   const [colors, setColors] = useState<Color[]>([]);
+
+  const loadColors = async () => {
+    const colorData = await AsyncStorage.getItem('@ColorListStore:Colors');
+    if (colorData) {
+      const colors: Color[] = JSON.parse(colorData);
+      setColors(colors);
+    }
+  };
+
+  useEffect(() => {
+    if (colors.length) return;
+    loadColors();
+  }, []);
+
+  useEffect(() => {
+    AsyncStorage.setItem('@ColorListStore:Colors', JSON.stringify(colors));
+  }, [colors]);
+
   const addColor = (color: string) => {
     const newColor: Color = { id: uuidv4(), color };
     setColors([newColor, ...colors]);
