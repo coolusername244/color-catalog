dee915c583c079cc282d9d7e7d97dbf82274bb96
Lee <leesheppard2404@gmail.com>
2024-01-16 14:24:06 +0100

    refactor: changed buttons into reusable components

--- App.tsx
+++ App.tsx
@@ -1,21 +1,39 @@
 import React, { useState } from 'react';
 import { StyleSheet, View, Text, TouchableHighlight } from 'react-native';
 
+type ColorButtonProps = {
+  backgroundColor: string;
+  onPress: (arg0: string) => void;
+};
+
 export default function App() {
   const [backgroundColor, setBackgroundColor] = useState('blue');
 
-  return (
-    <View style={[styles.container, { backgroundColor }]}>
+  const ColorButton: React.FC<ColorButtonProps> = ({
+    backgroundColor,
+    onPress,
+  }) => {
+    return (
       <TouchableHighlight
         style={styles.button}
-        onPress={() => setBackgroundColor('yellow')}
+        onPress={() => onPress(backgroundColor)}
         underlayColor="orange"
       >
         <View style={styles.row}>
-          <View style={[styles.sample, { backgroundColor: 'yellow' }]} />
-          <Text style={styles.buttonText}>Yellow</Text>
+          <View style={[styles.sample, { backgroundColor }]} />
+          <Text style={styles.buttonText}>{backgroundColor}</Text>
         </View>
       </TouchableHighlight>
+    );
+  };
+
+  return (
+    <View style={[styles.container, { backgroundColor }]}>
+      <ColorButton backgroundColor="red" onPress={setBackgroundColor} />
+      <ColorButton backgroundColor="green" onPress={setBackgroundColor} />
+      <ColorButton backgroundColor="blue" onPress={setBackgroundColor} />
+      <ColorButton backgroundColor="yellow" onPress={setBackgroundColor} />
+      <ColorButton backgroundColor="purple" onPress={setBackgroundColor} />
     </View>
   );
 }
